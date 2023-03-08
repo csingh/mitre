6c22c1751dcc6444ef35696b1366f438960e8e01
Chandeep Singh <csingh@stripe.com>
2023-03-08 13:34:59 -0500

    Add match_entities method

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,3 +1,69 @@
 class Sentence < ApplicationRecord
 	has_many :entities
+
+	# Returns a list of entity matches for the sentence, including the characters to highlight
+	def match_entities
+		# TODO: handle entity highlight collisions
+	    # E.g. if there are 2 entities with text: "Apple" and "Apple Org", how to figure out which is applied first
+	    # Possible hacky solution: When doing replacements, do a gsub to input some special markers like @@@ on both ends. If marker is found in a later match then need to figure out which entity is a subset of another
+
+	    all_matches = []
+
+		self.entities.each do |e|
+			# Not the most intuitive, but a nice way to get MatchData for all string matches
+			matches = self.text.to_enum(:scan, /#{e.text}/i).map { Regexp.last_match }
+
+			matches.each do |m|
+				first = m.begin(0)
+				last = m.end(0) - 1
+				all_matches << {
+					first: first,
+					last: last,
+					text: self.text[first..last],
+					type: e.entity_type,
+				}
+			end
+		end
+
+		all_matches
+	end
+
+	# Construct a list representation of the sentence, separating out tagged and untagged portions
+	def construct_tagged_text(entity_matches)
+		tagged_sentence = []
+
+		# i = sentence position
+		i = 0
+		while i < self.text.length
+			if entity_matches.empty?
+				tagged_sentence << {
+					tagged: false,
+					text: self.text[i..-1]
+				}
+				break
+			end
+
+			next_entity = entity_matches.first
+			if i == next_entity[:first]
+				# If we're at a currently matched entity
+				tagged_sentence << {
+					tagged: true,
+					text: next_entity[:text],
+					type: next_entity[:type],
+				}
+				i = next_entity[:last] + 1
+				entity_matches.shift
+			else
+				# Else untagged text until the next entity
+				tagged_sentence << {
+					tagged: false,
+					text: self.text[i..next_entity[:first]-1]
+				}
+				i = next_entity[:first]
+			end
+
+		end
+
+		tagged_sentence
+	end
 end
--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -13,8 +13,23 @@ RSpec.describe Sentence, type: :model do
       e1 = Entity.create!(text: "one", entity_type: "MONEY", sentence: s)
       e2 = Entity.create!(text: "two", entity_type: "GPE", sentence: s)
 
-      expect(s.entities.count).to equal(2)
+      expect(s.entities.count).to eq(2)
       expect(s.entities.map(&:text)).to match_array([e1.text, e2.text])
+    end
+  end
+
+  describe "#match_entities" do
+    it "returns matched entities and characters to highlight" do
+      s = Sentence.create!(text: "One two three one")
+      e1 = Entity.create!(text: "one", entity_type: "MONEY", sentence: s)
+      e2 = Entity.create!(text: "two", entity_type: "GPE", sentence: s)
+
+      matched_entities = s.match_entities
+      
+      expect(matched_entities.length).to eq(3)
+      expect(matched_entities[0]).to eq(first: 0, last: 2, text: "One", type: "MONEY")
+      expect(matched_entities[1]).to eq(first: 14, last: 16, text: "one", type: "MONEY")
+      expect(matched_entities[2]).to eq(first: 4, last: 6, text: "two", type: "GPE")
     end
   end
 end
