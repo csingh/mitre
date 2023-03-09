6fa64d888a020f43f0ee7a818644ecf44d4df4e9
Chandeep Singh <csingh@stripe.com>
2023-03-09 09:46:03 -0500

    Add a simple back button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post 'entity/create'
-  get '/sentences', to: 'sentence#index'
+  get '/sentences', to: 'sentence#index', as: 'sentences_list'
   get '/sentence/:id', to: 'sentence#show', as: 'sentence'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
