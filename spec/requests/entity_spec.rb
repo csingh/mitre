093227d22c6041c0b01058990b13eba33e5f2cbe
Chandeep Singh <csingh@stripe.com>
2023-03-09 01:30:53 -0500

    fix entity spec

--- spec/requests/entity_spec.rb
+++ spec/requests/entity_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Entities", type: :request do
   describe "GET /create" do
     it "returns http success" do
-      get "/entity/create"
+      post "/entity/create"
       expect(response).to have_http_status(:success)
     end
   end
