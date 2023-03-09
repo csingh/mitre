61832d836d847469882810f01ca4dfe5b77b5505
Chandeep Singh <csingh@stripe.com>
2023-03-09 09:40:25 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+seed_data = [
+	{
+		sentence: "Apple is looking at buying U.K. startup for $1 billion.",
+		entities: [
+			{ text: "Apple", type: "ORG" },
+			{ text: "U.K.", type: "GPE"},
+			{ text: "$1 billion", type: "MONEY" }
+		]
+	},
+	{
+		sentence: "Regional funds with exposure to United States and outperform equity market over 3 year",
+		entities: [
+			{ text: "Regional funds", type: "THEME"},
+			{ text: "United States", type: "GPE"},
+			{ text: "equity market", type: "THEME"},
+			{ text: "3 year", type: "TIME" }
+		]
+	}
+]
+
+seed_data.each do |data|
+	s = Sentence.create!(text: data[:sentence])
+	data[:entities].each do |e|
+		s.entities.create!(text: e[:text], entity_type: e[:type])
+	end
+end
